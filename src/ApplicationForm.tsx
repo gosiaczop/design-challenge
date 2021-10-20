b894498f4d80e780217856c13ab916ab931cfdd6
gosiaczop <75136419+gosiaczop@users.noreply.github.com>
2021-10-20 10:13:36 +0200

    Update ApplicationForm.tsx

--- src/ApplicationForm.tsx
+++ src/ApplicationForm.tsx
@@ -5,6 +5,90 @@ interface Props {}
 
 export const ApplicationForm = (props: Props) => (
     <div className={styles.exampleClass}>
-      Please put the form here
-    </div>
+    <div class="form-container">
+      <form class="register-form">
+        <input
+          id="first-name"
+          class="form-field"
+          type="text"
+          placeholder="First Name"
+          name="firstName"
+        />{" "}
+        <input
+          id="last-name"
+          class="form-field"
+          type="text"
+          placeholder="Last Name"
+          name="lastName"
+        />
+        <br />
+        <input
+          id="phone-number"
+          class="form-field"
+          type="text"
+          placeholder="Phone Number"
+          name="Phone Number"
+        />{" "}
+        <input
+          id="e-mail"
+          class="form-field"
+          type="text"
+          placeholder="E-mail"
+          name="e-mail"
+        />
+        <br />
+        <input
+          id="linkedin"
+          class="long"
+          type="text"
+          placeholder="Linkedin URL"
+          name="Linkedin URL"
+        />
+        <br />
+        <input
+          id="Potfolio"
+          class="long"
+          type="text"
+          placeholder="Potfolio"
+          name="Potfolio"
+        />
+        <br />
+        <input
+          id="Available"
+          class="form-field"
+          type="text"
+          placeholder="Available"
+          name="Availablee"
+        />{" "}
+        <input
+          id="Salary"
+          class="form-field"
+          type="text"
+          placeholder="Monthly Gross Salary"
+          name="Monthly Gross Salary"
+        />{" "}
+        <br />
+        <input
+          id="resume"
+          class="form-fieldfile"
+          type="file"
+          placeholder="Resume"
+          name="resume"
+        />{" "}
+        <input
+          id="other"
+          class="form-fieldfile"
+          type="file"
+          placeholder="Other Files"
+          name="other"
+        />
+        <br />
+        <button class="submit" type="submit">
+          SUBMIT
+        </button>
+        <button class="cancel" type="cancel">
+          CANCEL
+        </button>
+      </form>
+     </div>
 );
