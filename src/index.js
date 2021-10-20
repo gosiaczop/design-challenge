3c9e3a5962ab519ce76935d42c7511d6097f9c2a
gosiaczop <75136419+gosiaczop@users.noreply.github.com>
2021-10-20 10:43:14 +0200

    Update and rename index.tsx to index.js

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.png";
+import JobForm from "./jobform.js";
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="App">
+      <img alt="logo1" className="logo" src={logo} />
+      <h1>UI/UX Designer (m/f/d)</h1>
+      <h2>Remote Full Time</h2>
+      <h3>Join Our Team:</h3>
+      <JobForm />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
